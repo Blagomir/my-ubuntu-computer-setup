c3b61941f39182085f875f58cfeb29bd37d7f78f
blagomir <blagomir.ivanov@gmail.com>
2020-12-06 17:59:51 +0000

    Adding some niceties

--- install.sh
+++ install.sh
@@ -3,9 +3,16 @@
 sudo apt-get update
 sudo apt-get upgrade
 
+# Install some libraries, packages and nice software in general
+sudo apt-get install gnome-tweaks
+
+
 # Show battery percentage on screen
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 
 # Suspend when closing the lid (laptop)
 echo >> /etc/systemd/logind.conf
 echo "HandleLidSwitch=suspend" >> /etc/systemd/logind.conf
+
+# Minimise windows on click
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
